9f8fb22a5c915d0d769bffbe5ef98ce7092811ba
kenny <kenny@ecgoevmoto.com>
2025-11-25 11:09:30 +0700

    🚧 add theme color & typography services

---
+++ lib/app/services/theme_color_services.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ThemeColorServices extends GetxService {
+  final primaryBlue = Color(0XFF0060C6).obs;
+  final sematicColorBlue200 = Color(0XFF7AC0F8).obs;
+  final sematicColorYellow300 = Color(0XFFFFAD33).obs;
+  final sematicColorRed400 = Color(0XFFF44336).obs;
+  final neutralsColorGrey200 = Color(0XFFE5E5E5).obs;
+  final neutralsColorGrey700 = Color(0XFF2E2E2E).obs;
+  final neutralsColorSlate100 = Color(0XFFE0E3EB).obs;
+}
