3467e71f2aa0e0edf6292fa1dbcaa0b869762551
kenny <kenny@ecgoevmoto.com>
2025-12-02 14:02:08 +0700

    🎨 add one-time onboarding introduction

--- lib/app/modules/onboarding_introduction/controllers/onboarding_introduction_controller.dart
+++ lib/app/modules/onboarding_introduction/controllers/onboarding_introduction_controller.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:new_evmoto_user/app/routes/app_pages.dart';
 import 'package:new_evmoto_user/app/services/theme_color_services.dart';
 import 'package:new_evmoto_user/app/services/typography_services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class OnboardingIntroductionController extends GetxController {
   final themeColorServices = Get.find<ThemeColorServices>();
@@ -24,10 +25,12 @@ class OnboardingIntroductionController extends GetxController {
     super.onClose();
   }
 
-  onTapNext() {
+  Future<void> onTapNext() async {
     if (pageNumber.value == 0) {
       pageNumber.value = 1;
     } else if (pageNumber.value == 1) {
+      var prefs = await SharedPreferences.getInstance();
+      await prefs.setBool('is_onboarding_introduction_shown', true);
       Get.offAndToNamed(Routes.LOGIN_REGISTER);
     }
   }
--- lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
+++ lib/app/modules/splash_screen/controllers/splash_screen_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:new_evmoto_user/app/routes/app_pages.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreenController extends GetxController {
   final isFetch = false.obs;
@@ -14,7 +15,15 @@ class SplashScreenController extends GetxController {
       var token = await storage.read(key: 'token');
 
       if (token == null || token == "") {
-        Get.offAndToNamed(Routes.ONBOARDING_INTRODUCTION);
+        var prefs = await SharedPreferences.getInstance();
+        var isOnboardingIntroductionShown = prefs.getBool(
+          'is_onboarding_introduction_shown',
+        );
+        if (isOnboardingIntroductionShown == true) {
+          Get.offAndToNamed(Routes.LOGIN_REGISTER);
+        } else {
+          Get.offAndToNamed(Routes.ONBOARDING_INTRODUCTION);
+        }
       } else {
         Get.offAndToNamed(Routes.HOME);
       }
