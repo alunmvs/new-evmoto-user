de3d8d1c1d624c2f34f475e98580c5c8a3d026a7
kenny <kenny@ecgoevmoto.com>
2025-12-02 17:33:51 +0700

    🎨 add API geocode, direction, and helper

--- lib/app/repositories/google_maps_repository.dart
+++ lib/app/repositories/google_maps_repository.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:new_evmoto_user/app/data/google_direction_model.dart';
+import 'package:new_evmoto_user/app/data/google_geo_code_search_model.dart';
 import 'package:new_evmoto_user/app/data/google_place_text_search_model.dart';
 
 class GoogleMapsRepository {
@@ -12,13 +14,77 @@ class GoogleMapsRepository {
       var dio = Dio();
       var response = await dio.get(
         url,
-        queryParameters: {"query": query, "language": "en", "key": ""},
+        queryParameters: {"query": query, "language": language, "key": ""},
       );
 
       var result = <GooglePlaceTextSearch>[];
 
       for (var place in response.data['results']) {
         result.add(GooglePlaceTextSearch.fromJson(place));
+      }
+
+      return result;
+    } on DioException catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<GoogleGeoCodeSearch>>
+  getRecommendationPlaceListByLatitudeLongitude({
+    String? latitude,
+    String? longitude,
+    String? language,
+  }) async {
+    try {
+      var url = "https://maps.googleapis.com/maps/api/geocode/json";
+
+      var dio = Dio();
+      var response = await dio.get(
+        url,
+        queryParameters: {
+          "latlng": "$latitude,$longitude",
+          "language": language,
+          "key": "",
+        },
+      );
+
+      var result = <GoogleGeoCodeSearch>[];
+
+      for (var place in response.data['results']) {
+        result.add(GoogleGeoCodeSearch.fromJson(place));
+      }
+
+      return result;
+    } on DioException catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<GoogleDirection>> getDirection({
+    String? originLatitude,
+    String? originLongitude,
+    String? destinationLatitude,
+    String? destinationLongitude,
+    String? region,
+  }) async {
+    try {
+      var url = "https://maps.googleapis.com/maps/api/directions/json";
+
+      var dio = Dio();
+      var response = await dio.get(
+        url,
+        queryParameters: {
+          "origin": "$originLatitude,$originLongitude",
+          "destination": "$destinationLatitude,$destinationLongitude",
+          "language": "en",
+          "key": "",
+        },
+      );
+
+      var result = <GoogleDirection>[];
+
+      for (var place in response.data['routes']) {
+        result.add(GoogleDirection.fromJson(place));
       }
 
       return result;
---
+++ lib/app/utils/bitmap_descriptor_helper.dart
@@ -0,0 +1,37 @@
+import 'dart:math' as math;
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class BitmapDescriptorHelper {
+  static Future<BitmapDescriptor> getBitmapDescriptorFromSvgAsset(
+    String assetName, [
+    Size size = const Size(48, 48),
+  ]) async {
+    final pictureInfo = await vg.loadPicture(SvgAssetLoader(assetName), null);
+
+    double devicePixelRatio = ui.window.devicePixelRatio;
+    int width = (size.width * devicePixelRatio).toInt();
+    int height = (size.height * devicePixelRatio).toInt();
+
+    final scaleFactor = math.min(
+      width / pictureInfo.size.width,
+      height / pictureInfo.size.height,
+    );
+
+    final recorder = ui.PictureRecorder();
+
+    ui.Canvas(recorder)
+      ..scale(scaleFactor)
+      ..drawPicture(pictureInfo.picture);
+
+    final rasterPicture = recorder.endRecording();
+
+    final image = rasterPicture.toImageSync(width, height);
+    final bytes = (await image.toByteData(format: ui.ImageByteFormat.png))!;
+
+    return BitmapDescriptor.fromBytes(bytes.buffer.asUint8List());
+  }
+}
