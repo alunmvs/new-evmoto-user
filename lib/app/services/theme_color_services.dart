9b8d829c0fb25c53b2e3d6cd7ef44781c34c2233
kenny <kenny@ecgoevmoto.com>
2025-12-02 17:34:05 +0700

    🎨 add new color

--- lib/app/services/theme_color_services.dart
+++ lib/app/services/theme_color_services.dart
@@ -11,6 +11,7 @@ class ThemeColorServices extends GetxService {
   final sematicColorGreen500 = Color(0XFF17412C).obs;
   final sematicColorBlue100 = Color(0XFFCFE9FC).obs;
   final sematicColorBlue200 = Color(0XFF7AC0F8).obs;
+  final sematicColorBlue300 = Color(0XFF4DABF5).obs;
   final sematicColorBlue400 = Color(0XFF2196F3).obs;
   final sematicColorBlue500 = Color(0XFF145F9A).obs;
   final sematicColorBlue600 = Color(0XFF072841).obs;
