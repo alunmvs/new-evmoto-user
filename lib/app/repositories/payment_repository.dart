ce06d7db8be6f858da94824d3bd3aae69092c1b3
kenny <kenny@ecgoevmoto.com>
2025-12-15 09:34:58 +0700

    🎨 add API balance history deposit and consumption

--- lib/app/repositories/payment_repository.dart
+++ lib/app/repositories/payment_repository.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart' hide FormData;
+import 'package:new_evmoto_user/app/data/balance_history_consumption_model.dart';
+import 'package:new_evmoto_user/app/data/balance_history_deposit_model.dart';
 import 'package:new_evmoto_user/app/data/deposit_balance_model.dart';
 import 'package:new_evmoto_user/app/services/api_services.dart';
 import 'package:new_evmoto_user/app/services/firebase_remote_config_services.dart';
@@ -88,6 +90,102 @@ class PaymentRepository {
         data: formData,
         options: Options(headers: headers),
       );
+    } on DioException catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<BalanceHistoryDeposit>> getBalanceHistoryDeposit({
+    required int pageNum,
+    required int size,
+    required int language,
+  }) async {
+    try {
+      var url =
+          "${firebaseRemoteConfigServices.remoteConfig.getString("user_base_url")}/user/api/recharge/queryMyTravelRecord";
+
+      var formData = FormData.fromMap({
+        "language": language,
+        "pageNum": pageNum,
+        "size": size,
+      });
+
+      var storage = FlutterSecureStorage();
+      var token = await storage.read(key: 'token');
+
+      var headers = {
+        "Content-Type": "multipart/form-data",
+        'Authorization': "Bearer $token",
+      };
+
+      var dio = apiServices.dio;
+      var response = await dio.post(
+        url,
+        data: formData,
+        options: Options(headers: headers),
+      );
+
+      if (response.data['code'] != 200) {
+        throw response.data['msg'];
+      }
+
+      var balanceHistoryDepositList = <BalanceHistoryDeposit>[];
+
+      for (var balanceHistoryDeposit in response.data['data']) {
+        balanceHistoryDepositList.add(
+          BalanceHistoryDeposit.fromJson(balanceHistoryDeposit),
+        );
+      }
+
+      return balanceHistoryDepositList;
+    } on DioException catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<List<BalanceHistoryConsumption>> getBalanceHistoryConsumption({
+    required int pageNum,
+    required int size,
+    required int language,
+  }) async {
+    try {
+      var url =
+          "${firebaseRemoteConfigServices.remoteConfig.getString("user_base_url")}/user/api/recharge/queryMyTravelRecord";
+
+      var formData = FormData.fromMap({
+        "language": language,
+        "pageNum": pageNum,
+        "size": size,
+      });
+
+      var storage = FlutterSecureStorage();
+      var token = await storage.read(key: 'token');
+
+      var headers = {
+        "Content-Type": "multipart/form-data",
+        'Authorization': "Bearer $token",
+      };
+
+      var dio = apiServices.dio;
+      var response = await dio.post(
+        url,
+        data: formData,
+        options: Options(headers: headers),
+      );
+
+      if (response.data['code'] != 200) {
+        throw response.data['msg'];
+      }
+
+      var balanceHistoryConsumptionList = <BalanceHistoryConsumption>[];
+
+      for (var balanceHistoryConsumption in response.data['data']) {
+        balanceHistoryConsumptionList.add(
+          BalanceHistoryConsumption.fromJson(balanceHistoryConsumption),
+        );
+      }
+
+      return balanceHistoryConsumptionList;
     } on DioException catch (e) {
       rethrow;
     }
