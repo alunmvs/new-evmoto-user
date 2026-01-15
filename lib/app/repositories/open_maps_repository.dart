38cd413c5e9696f35a50b444b2d9885efd6c8e3c
kenny <kenny@ecgoevmoto.com>
2026-01-15 18:23:55 +0700

    🎨 update api routing

--- lib/app/repositories/open_maps_repository.dart
+++ lib/app/repositories/open_maps_repository.dart
@@ -1,10 +1,13 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:new_evmoto_user/app/data/models/open_map_direction_model.dart';
 import 'package:new_evmoto_user/app/services/api_services.dart';
+import 'package:new_evmoto_user/app/services/firebase_remote_config_services.dart';
 
 class OpenMapsRepository {
   final apiServices = Get.find<ApiServices>();
+  final firebaseRemoteConfigServices = Get.find<FirebaseRemoteConfigServices>();
 
   Future<OpenMapDirection> getDirection({
     String? originLatitude,
@@ -14,11 +17,17 @@ class OpenMapsRepository {
   }) async {
     try {
       var url =
-          "https://router.project-osrm.org/route/v1/driving/$originLongitude,$originLatitude;$destinationLongitude,$destinationLatitude";
+          "${firebaseRemoteConfigServices.remoteConfig.getString("user_base_url")}/businessProcess/api/osrm/route/v1/driving/route/v1/driving/$originLongitude,$originLatitude;$destinationLongitude,$destinationLatitude";
+
+      var storage = FlutterSecureStorage();
+      var token = await storage.read(key: 'token');
+
+      var headers = {'Authorization': "Bearer $token"};
 
       var dio = apiServices.dio;
       var response = await dio.get(
         url,
+        options: Options(headers: headers),
         queryParameters: {"overview": "full", "geometries": "geojson"},
       );
 
